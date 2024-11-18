7aeddcc2ba5f52055b06a1128e525f681668e923
Rob Thomas <14227@coderacademy.edu.au>
2024-11-18 20:58:44 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,108 @@
+# # print(2 + 3)
+
+# # # The below line prints Hello World
+# # # The Python interpreter will ignore comments
+# # print("Hello World!!")
+
+# # print("Goodbye!") # See ya!
+
+# # Data Types
+
+
+
+# # Numbers
+
+# # int
+# # 5
+# # 1000
+# print(type(5))
+
+# # float
+# # 2.5
+# # 5.3
+# # 3.14159
+
+# print(type(3.14159))
+
+# # complex
+# # 2 + 3j
+# print(type(2 + 3j))
+
+# # Number Operations
+# print(2 + 3)
+# print(2 - 3)
+# print(5 * 2) # Multiplication
+# print(10 / 5) # Floating Point Division
+# print(10 // 5) # Integer Division ... sometimes
+# print(int(10 // 3.5)) # Integer Division (with typecast)
+# print(5 % 2) # Modulo (remainder)
+
+
+# # Strings
+
+# # "Hello World!!"
+# # 'Hello World!!'
+# print('Hello!!')
+# print("Hello!!")
+# print("It's Monday")
+# print('It\'s Monday')
+# print(type('Hello'))
+
+# # Concatenation
+
+# print("Hello" + " World!!")
+# print("Hello" + " " + "World!!")
+
+# # String methods
+
+# print("Hello".upper())
+# print("Hello".lower())
+# print("Hello World".replace("World", "There"))
+
+# # Boolean
+
+# # True
+# # False
+# print(True)
+# print(False)
+# print(type(True))
+
+# # Falsy values
+
+# print(bool(0))
+# print(bool(""))
+# print(bool(None))
+# print(bool([]))
+# print(bool(()))
+# print(bool({}))
+
+# # Comparison operators
+
+# # <, >, <=, >=, ==
+# print(2 < 3)
+# print(2 > 3)
+# print(5 >= 5)
+# print(5 == 5)
+
+# # Logical operators
+
+# # and, or, not
+# print(True and False)
+# print(True or False)
+# print(5 > 3 and 2 < 5)
+# print(5 < 3 and 8 > 1)
+
+# Variables
+
+# num1 = 5
+# num2 = 4
+# print(num1 + num2)
+# num2 = 10
+# print(num1 + num2)
+
+# String interpolation
+
+name = "Rob"
+print("My name is " + name + " and I live in Canberra")
+# f-string
+print(f"My name is {name} and I live in Canberra")
